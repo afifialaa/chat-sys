f4861e5d33f829e0d8b428279e6ce07daed0c934
alaa afifi <alaaahmedafifi@gmail.com>
2022-06-08 22:30:06 +0200

    messages action filter

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,52 +1,26 @@
 class Api::V1::MessagesController < ApplicationController
 
     skip_before_action :verify_authenticity_token
+    before_action :set_application
+    before_action :set_chat
 
     def index
-        @application = Application.find_by(token: params[:token])
-        @chat = Application.chats.find_by(number: params[:number])
         @messages = @chat.messages
     end
 
     def create
-        @application = Application.find_by(token: params[:token])
-        if @application == nil 
-            render(json: {msg: "application not found"}, status: :not_found)
-            return
-        end
-
-        @chat = @application.chats.find_by(number: params[:chat_number])
-
-        if @chat == nil 
-            render(json: {msg: "chat not found"}, status: :not_found)
-            return
-        end
-
         @message = @chat.messages.create(content: params[:content])
 
         if @message
             render(json: {"number": @message.number}, status: :created)
-            return
         else
             render(json: {}, status: :internal_server_error)
-            return
         end
     end
 
     def delete
-        @application = Application.find_by(token: params[:token])
-        if @application == nil
-            render(json: {"error": "application not found"}, status: :not_found)
-            return
-        end
-
-        @chat = @application.chats.find_by(number: params[:chat_number])
-        if @chat == nil
-            render(json: {"error":"chat not found"}, status: :not_found)
-            return
-        end
-
         @message = @chat.messages.find_by(number: params[:message_number])
+
         if @message == nil
             render(json: {"error": "message not found" }, status: :not_found)
             return
@@ -60,11 +34,7 @@ class Api::V1::MessagesController < ApplicationController
     end
     
     def update
-        @application = Application.find_by(token: params[:token])
-        # @chat = @application.chats.find_by(number: params[:chat_number])
-        @chat = @application.chats.find(params[:chat_number])
-        # @message = @chat.messages.find_by(number: params[:message_number])
-        @message = @chat.messages.find(params[:message_number])
+        @message = @chat.messages.find_by(number: params[:message_number])
 
         if @message.update(message_params)
             render(json: {}, status: :ok)
@@ -74,24 +44,15 @@ class Api::V1::MessagesController < ApplicationController
     end
 
     def show
-        @application = Application.find_by(token: params[:token])
-        # @chat = @application.chats.find_by(number: params[:chat_number])
-        @chat = @application.chats.find(params[:chat_number])
-        # @message = @chat.messages.find_by(number: params[:message_number])
         @message = @chat.messages.find(params[:message_number])
 
         render(json: @message.as_json(:except => :id), status: :internal_server_error)
     end
 
     def partial_search
-        @application = Application.find_by(token: params[:token])
-        #@chat = @application.chats.find_by(number: params[:chat_number])
-        @chat = @application.chats.find(params[:chat_number])
-
         @message = Message.partial_search(params[:query], 10)
-        puts @message
 
-        render(json: {"msg": @message}, status: :ok)
+        render(json: {"result": @message}, status: :ok)
     end
 
     private
